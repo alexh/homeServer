cf5e8c3f32e0f17d0f8bda075dc2a93c1218688d
alexhaynes32 <alhaynes@seas.upenn.edu>
2017-01-29 12:09:54 -0500

    Added package.json & readme

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+console.log('Booting up');
+
+var server = http.createServer(function(request, response){
+	console.log('got a request!');
+	response.write('Hi!');
+	response.end();
+});
+
+server.listen(8000);
