a32948fa7caed03d426ceb45c8826677ddd7cd0e
alexhaynes32 <alhaynes@seas.upenn.edu>
2017-01-31 11:25:06 -0500

    made port a variable

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require('http');
 
-console.log('Booting up');
-
 var server = http.createServer(function(request, response){
 	console.log('got a request!');
 	response.write('Hi!');
 	response.end();
 });
 
-server.listen(3000);
+var port = 3000;
+server.listen(port);
+
+console.log('running on port: ' + port)
